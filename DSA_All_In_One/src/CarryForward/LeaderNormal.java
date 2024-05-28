6fa4a779cc58f0cc89f7decb6c958f55c597fedd
Abhi <abhikshirsagar1999@gmail.com>
2024-05-28 07:23:12 +0530

    Carry Forward one question added.

---
+++ DSA_All_In_One/src/CarryForward/LeaderNormal.java
@@ -0,0 +1,63 @@
+package CarryForward;
+
+/*
+ Given an integer array A containing N distinct integers, you have to find all the leaders in array A. 
+ An element is a leader if it is strictly greater than all the elements to its right side.
+
+NOTE: The rightmost element is always a leader.
+ */
+public class LeaderNormal {
+	// Problem facing with this method for skipping 4.
+	public void findLeaders(int A[]) {
+		for (int i = 0; i < A.length; i++) {
+			int temp = A[i];
+
+			boolean isVisited = false;
+			for (int j = i + 1; j < A.length; j++) {
+				if (temp < A[j]) {
+
+					break;
+				} else if (temp > A[j]) {
+
+					if (!isVisited) {
+						System.out.println(temp);
+					}
+					isVisited = true;
+				}
+			}
+		}
+	}
+// Correct implementation and 
+	public void findLeadersTwo(int arr[]) {
+		int n = arr.length;
+
+		// Rightmost element is always a leader
+		System.out.print(arr[n - 1] + " ");
+
+		// Traverse from n-2 to 0
+		for (int i = n - 2; i >= 0; i--) {
+			int current = arr[i];
+			boolean isLeader = true;
+
+			// Check if current is greater than all elements on its right
+			for (int j = i + 1; j < n; j++) {
+				if (current < arr[j]) {
+					isLeader = false;
+					break;
+				}
+			}
+
+			if (isLeader) {
+				System.out.print(current + " ");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LeaderNormal ln = new LeaderNormal();
+		int A[] = { 16, 17, 4, 3, 5, 2 };
+		ln.findLeadersTwo(A);
+	}
+
+}
